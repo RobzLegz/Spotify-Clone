8e7c3abee24b42c11a0d1d97ec20d2920640727e
Robzlegz <robzlegz@gmail.com>
2021-01-29 22:14:06 +0200

    some styling

--- src/Sidebar.js
+++ src/Sidebar.js
@@ -1,14 +1,22 @@
 import React from 'react';
 import "./Sidebar.css";
 import SidebarOption from './SidebarOption';
+import HomeIcon from '@material-ui/icons/Home';
+import SearchIcon from '@material-ui/icons/Search';
+import LibraryMusicIcon from '@material-ui/icons/LibraryMusic';
 
 const Sidebar = () => {
     return (
         <div className="sidebar">
             <img src="https://upload.wikimedia.org/wikipedia/commons/2/26/Spotify_logo_with_text.svg" alt="sidebar logo" className="sidebar-logo"/>
-            <SidebarOption title="Home" />
-            <SidebarOption title="Search" />
-            <SidebarOption title="Your Library"  />
+            <SidebarOption title="Home" Icon={HomeIcon} />
+            <SidebarOption title="Search" Icon={SearchIcon} />
+            <SidebarOption title="Your Library" Icon={LibraryMusicIcon} />
+            <strong className="sidebar-title">PLAYLISTS</strong>
+            <hr/>
+            <SidebarOption title="Hip hop" />
+            <SidebarOption title="Hip hop" />
+            <SidebarOption title="Hip hop" />
         </div>
     );
 }
