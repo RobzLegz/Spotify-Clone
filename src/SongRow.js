1f527662037afa38f62d0187844f44ec11028e9f
Robzlegz <robzlegz@gmail.com>
2021-01-30 16:55:45 +0200

    code with styling issues

--- src/SongRow.js
+++ src/SongRow.js
@@ -1,9 +1,15 @@
 import React from 'react';
 
-const SongRow = () => {
+const SongRow = ({track="test"}) => {
     return (
-        <div>
-            dfdsf
+        <div className="song-row">
+            <div className="song-row-info">
+                <h1>{track.name}</h1>
+                <p>
+                    {track.artists.map((artist) => artist.name)}
+                    {track.album.name}
+                </p>
+            </div>
         </div>
     );
 }
